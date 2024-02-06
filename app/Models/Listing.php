a8b70e5d97b85c2e998a675840b88b2a78767acd
donmoreno09 <morenodaqm@gmail.com>
2024-02-06 10:37:02 +0100

    added some files

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,30 +1,11 @@
-<?php 
+<?php
+
 namespace App\Models;
 
-class Listing{
-    public static function all(){
-        return [
-            [
-                'id' => '1',
-                'title' => 'Listing One',
-                'description' => 'WAZZZZZZZZZZZZZZZZUP'
-            ],
-            [
-                'id' => '2',
-                'title' => 'Listing Two',
-                'description' => 'WAZZZZZZZZZZZZZZZZUUUUUUUUUUUUUUUUUUUUUUUUUP'
-            ]
-        ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id){
-        $listings = self::all();
-
-        foreach($listings as $listing){
-            if($listing['id'] == $id){
-                return $listing;
-            }
-        }
-    }
+class Listing extends Model
+{
+    use HasFactory;
 }
-
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Listing;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Listing::create([
+            'title' => 'Laravel Senior Development',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Boston, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'description'
+        ]);
+
+        Listing::create([
+            'title' => 'Full-stack Developer',
+            'tags' => 'laravel, javascript',
+            'company' => 'Acme Corp',
+            'location' => 'Milan, MA',
+            'email' => 'email1@email.com',
+            'website' => 'https://www.acme.com',
+            'description' => 'description'
+        ]);
     }
 }
