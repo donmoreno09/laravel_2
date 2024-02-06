d7155452ebee0021b8cfbd5ef24db430eb392c51
donmoreno09 <morenodaqm@gmail.com>
2024-02-06 10:17:27 +0100

    laravelProva con Traversy Media Youtube

---
+++ resources/views/singleListing.blade.php
@@ -0,0 +1,3 @@
+
+<h2> {{$listings['title']}} </h2>
+<p> {{$listings['description']}}</p>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Listing;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,50 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */ 
+
+Route::get('/', function () {
+    return view('listings', [
+        'heading' => 'List of Listings',
+        'listings' => Listing::all() /*[
+            /*[
+                'id' => '1',
+                'title' => 'Listing One',
+                'description' => 'WAZZZZZZZZZZZZZZZZUP'
+            ],
+            [
+                'id' => '2',
+                'title' => 'Listing Two',
+                'description' => 'WAZZZZZZZZZZZZZZZZUUUUUUUUUUUUUUUUUUUUUUUUUP'
+            ]*/
+    ]);
+});
+
+Route::get('singleListing/{id}', function($id) {
+    return view('singleListing', [
+        'listings' => Listing::find($id)
+    ]);
+
+});
+
+Route::get('hello', function () {
+    return response('<h1>Hello World</h1>');
+});
+
+Route::get('posts/{id}', function($id){
+    return response('Posts ' . $id); //response is a helper method
+})->where('id', '[0-9]+'); //costraints per poter ricevere solo numeri interi
+
+Route::get('posts1/{id}', function($id){
+    ddd($id); //die dump debug helper method, will stop everything and show what parameter i passed, debugging
+    return response('Posts ' . $id); //response is a helper method
+})->where('id', '[0-9]+'); //costraints per poter ricevere solo numeri interi
+
+
+Route::get('search', function (Request $request) {
+    //dd($request); //fa vedere una serie di robe
+    //dd($request->name . ' ' . $request->city);
+    return ($request->name . ' ' . $request->city);
 });
